12484793a7cefa14c4104ce613ae2e22fa1dfb28
Jim Newton <jnewton@marcello.lrde.epita.fr>
2020-07-30 16:35:31 +0200

    white space

--- cl-robdd-scala/src/test/scala/bdd/MapColoringTestSuite.scala
+++ cl-robdd-scala/src/test/scala/bdd/MapColoringTestSuite.scala
@@ -77,7 +77,8 @@ class MapColoringTestSuite extends AnyFunSuite {
   }
 
   test("usa"){
-    colorizeMap(30, "test-us", "AL", USAgraph.stateUniGraph,USAgraph.stateBiGraph,List("MS","AL","TN"))
+    colorizeMap(30, "test-us", "AL",
+                USAgraph.stateUniGraph,USAgraph.stateBiGraph,List("MS","AL","TN"))
   }
   def sanityCheck(numNodes:Int):Unit = {
     import accumulators.Accumulators._
