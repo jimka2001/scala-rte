69c9f9815102b513f81e1ade4f00c8b4707f44e8
Jim Newton <jnewton@lrde.epita.fr>
2020-11-21 14:47:58 +0100

    cnf instance variable and comments

--- cl-robdd-scala/src/main/scala/genusbdd/GenusBdd.scala
+++ cl-robdd-scala/src/main/scala/genusbdd/GenusBdd.scala
@@ -24,8 +24,19 @@ import genus._
 import genus.Types._
 import bdd._
 import scala.collection._
+import NormalForm._
 
 case class GenusBdd(td:SimpleTypeD,tdToInt:mutable.Map[SimpleTypeD,Int]) {
+  // Creation of a Bdd representing a SimpleTypeD
+  // To do this we associate each unique type within the SimpleTypeD by an
+  //   integer which we accumulate in the given tdToInt mutable.Map.
+  //   The caller should re-use this mutable map if desiring to create more
+  //   Bdds to combine together.   E.g., if the integer 12 corresponds to
+  //   type java.lang.Integer, then 12 needs to correspond also to
+  //   java.lang.Integer in any other Bdd in order to be compatible.
+
+  // bdd is the Bdd representation of the given SimpleTypeD, however,
+  //    the Bdd does not know anything about subtype/supertype relations
   lazy val bdd: Bdd = toBdd(td)
   def toBdd(td:SimpleTypeD):Bdd = {
     td match {
@@ -41,16 +52,36 @@ case class GenusBdd(td:SimpleTypeD,tdToInt:mutable.Map[SimpleTypeD,Int]) {
       case _ => ???
     }
   }
+  // dnf is a SimpleTypeD which represents the original td
+  //   but in a Disjunctive Normal Form.   The DNF is an Or of Ands
+  //   where each And has been reduced according to subtype/supertype
+  //   relations, and according to disjoint types.
   lazy val dnf: SimpleTypeD = locally{
     val m:immutable.Map[Int,SimpleTypeD] = tdToInt.map(_.swap).toMap
     GenusBdd.BddToDnf(bdd,m)
   }
+
+  // cnf is a SimpleTypeD which represents the original td
+  //   but in a Conjunctive Normal Form.  The CNF is an And
+  //   of Ors which has been computed by transforming the DNF.
+  lazy val cnf: SimpleTypeD = dnf.canonicalize(nf=Some(Cnf))
 }
 
 object GenusBdd {
 
   def BddToDnf(bdd: Bdd,
                intToTd        : immutable.Map[Int, SimpleTypeD]): SimpleTypeD = {
+    // We convert a Bdd to Dnf, using SimpleTypeD objects by starting at the
+    //   root node of the Bdd, and walking to every BddTrue child.  Along each
+    //   walk we accumulate a list of SimpleTypeD objects.  When passing through
+    //   a negative child, we invert the object using SNot().  When arriving
+    //   at a BddFalse terminal node, we do nothing, but terminate the recursion branch.
+    //   When arriving at a BddTrue terminal node, we convert the list of type descriptors
+    //   to their intersection using SAnd, and push this SimpleTypeD onto terms.
+    //   When finished, we then use SOr to union the SAnd objects in terms.
+    //   It might be some some objects in terms is not really an SAnd object because it
+    //   has been reduced with a call to the canonicalize method which might convert
+    //   it to a more fundamental type.
     var terms = List[SimpleTypeD]()
 
     def recur(bdd: Bdd, lineage: List[SimpleTypeD]): Any = {
@@ -88,11 +119,14 @@ object GenusBdd {
       tds.foreach { td => println(GenusBdd(td, tdToInt).bdd) }
       println(tdToInt)
       tds.foreach { td =>
-        val dnf = GenusBdd(td, tdToInt).dnf
+        val g = GenusBdd(td, tdToInt)
+        val dnf = g.dnf
+        val cnf = g.cnf
         val can = dnf.canonicalize()
         println() // blank
         println(s"td = $td")
         println(s"dnf           = $dnf")
+        println(s"cnf           = $cnf")
         println(s"canonicalized = $can")
 
       }
