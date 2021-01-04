8c95f6cdc74bc03e668d5e213761b0a8de832dca
Jim Newton <jnewton@lrde.epita.fr>
2021-01-04 10:37:52 +0100

    added test cases of inhabited method

--- cl-robdd-scala/src/test/scala/genus/GenusInhabited.scala
+++ cl-robdd-scala/src/test/scala/genus/GenusInhabited.scala
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 EPITA Research and Development Laboratory
+// Copyright (c) 2020,21 EPITA Research and Development Laboratory
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation
@@ -22,9 +22,7 @@
 
 package genus
 
-import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
-
 
 class GenusInhabited extends AnyFunSuite {
   class Abstract1
@@ -33,7 +31,42 @@ class GenusInhabited extends AnyFunSuite {
   trait Trait2
   trait Trait3
 
+  test("inhabited SEql"){
+    assert(SEql(3).inhabited.contains(true))
+    assert(SEql(false).inhabited.contains(true))
+    assert(SEql("hello").inhabited.contains(true))
+  }
+  test("inhabited SMember"){
+    assert(SMember().inhabited.contains(false))
+    assert(SMember(3).inhabited.contains(true))
+    assert(SMember(false).inhabited.contains(true))
+    assert(SMember("hello").inhabited.contains(true))
+    assert(SMember("hello","world").inhabited.contains(true))
+    assert(SMember("hello",3,false).inhabited.contains(true))
+  }
+  test("inhabited SNot"){
+    assert(SNot(SEmpty).inhabited.contains(true))
+    assert(SNot(STop).inhabited.contains(false))
+    assert(SNot(SEql(3)).inhabited.contains(true))
+    assert(SNot(SEql(false)).inhabited.contains(true))
+    assert(SNot(SEql("hello")).inhabited.contains(true))
+    assert(SNot(SMember()).inhabited.contains(true))
+    assert(SNot(SMember(3)).inhabited.contains(true))
+    assert(SNot(SMember(false)).inhabited.contains(true))
+    assert(SNot(SMember("hello")).inhabited.contains(true))
+    assert(SNot(SMember("hello","world")).inhabited.contains(true))
+    assert(SNot(SMember("hello",3,false)).inhabited.contains(true))
+  }
+  test("inhabited union"){
+    assert(SEmpty.inhabited.contains(false))
+    assert(STop.inhabited.contains(true))
+    assert(SOr(SEmpty,STop).inhabited.contains(true))
+    assert(SOr(SMember(),SMember(1,2,3)).inhabited.contains(true))
+    assert(SOr(SMember(2,3,4),SMember(1,2,3)).inhabited.contains(true))
+  }
   test("inhabited intersection") {
+    assert(SAnd(SEmpty,STop).inhabited.contains(false))
+    assert(SAnd(SMember(),SMember(1,2,3)).inhabited.contains(false))
     assert(SAnd(SAtomic(classOf[Trait1]),
                 SAtomic(classOf[Abstract1]),
                 SAtomic(classOf[Abstract2]),
