eaa02908058aabf520b4b9bc7ad438e109b26508
Jim Newton <jnewton@lrde.epita.fr>
2021-08-27 11:33:38 +0200

    updated tests

--- cl-robdd-scala/src/test/scala/genus/GenusCanonicalize.scala
+++ cl-robdd-scala/src/test/scala/genus/GenusCanonicalize.scala
@@ -386,20 +386,33 @@ class GenusCanonicalize extends AnyFunSuite {
     }
   }
   test("discovered 388"){
-    val one:Long = 1
-    assert(classOf[Long].isInstance(one))
-    assert(classOf[Integer].isInstance(one))
+    val one:Integer = 1
+    //assert(classOf[Long].isInstance(one))
+    //assert(classOf[Integer].isInstance(one))
+    assert(SAtomic(classOf[Integer]).typep(one))
     assert(SAtomic(Integer).typep(one))
+    check_type(SAtomic(Integer))
+    check_type(SOr(SAtomic(classOf[Integer]),SEql(one)))
     check_type(SOr(SAtomic(Integer),SEql(one)))
 
-    assert(SAtomic(Integer).typep(1))
+    assert(SAtomic(classOf[Integer]).typep(1))
+    check_type(SOr(Integer,SEql(1)))
     check_type(SOr(SAtomic(Integer),SEql(1)))
   }
   test("discovered 389"){
-    for{ one <- Seq(1:Long, 1:Integer, 1:Int, 1L, 1)} {
-      assert(classOf[Integer].isInstance(one))
-      assert(SAtomic(Integer).typep(one))
-    }
+    assert(SAtomic(classOf[Long]).typep(1L))
+    assert(SAtomic(classOf[scala.Long]).typep(1L))
+    assert(SAtomic(classOf[java.lang.Long]).typep(1L))
+
+    assert(SAtomic(classOf[Integer]).typep(1))
+    assert(SAtomic(classOf[scala.Int]).typep(1))
+    assert(SAtomic(classOf[java.lang.Integer]).typep(1))
+    assert(SAtomic(classOf[Int]).typep(1))
+    assert(SAtomic(classOf[Int]).typep(1L))
+
+    assert(SAtomic(classOf[Short]).typep(1:Short))
+    assert(SAtomic(classOf[java.lang.Short]).typep(1:Short))
+    assert(SAtomic(classOf[scala.Short]).typep(1:Short))
   }
   test("rand typep") {
     // make sure typep membership of particular values is the same before and after canonicalizing
