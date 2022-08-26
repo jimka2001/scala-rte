55919817d29a82f68da81c201f790a6c1eca1230
Axel Gautier <ax.gautier@yahoo.com>
2022-08-26 15:44:17 +0200

    removed all vars except from canonicalize, now going to try changing canonicalize

--- cl-robdd-scala/src/main/scala/nfa/Nfa.scala
+++ cl-robdd-scala/src/main/scala/nfa/Nfa.scala
@@ -31,9 +31,9 @@ object Nfa {
     {
       if(here.nonEmpty)
       {
-        return recur(here.flatMap(delta).diff(done),done++here)
+        recur(here.flatMap(delta).diff(done),done++here)
       }
-      done
+      else{done}
     }
     recur(initials,Set())
   }
@@ -47,12 +47,18 @@ object Nfa {
     {
       if(here.nonEmpty)
       {
-        return recur(here.flatMap(delta).diff(done),done++here)
+        recur(here.flatMap(delta).diff(done),done++here)
       }
-      done
+      else {done}
     }
     recur(finals,Set())
   }
+
+  def ftrans[L](useful : Set[Int], trans : (Int,L,Int)) : Boolean =
+  {
+    useful.contains(trans._1) && useful.contains(trans._3)
+  }
+
   def findUseless[L](initials: Set[Int], finals: Set[Int], transitions : Array[(Int, L, Int)])
   : Set[Int] = {
     val allstates = transitions.flatMap(tr=>Seq(tr._1,tr._3)).toSet
@@ -60,21 +66,13 @@ object Nfa {
   }
   def remove[L](initials: Set[Int], finals: Set[Int], transitions : Array[(Int, L, Int)])
   : (Set[Int], Set[Int], Array[(Int, L, Int)]) = {
-    val allstates = initials ++ finals ++ transitions.flatMap(tr=>Seq(tr._1,tr._3)).toSet
-    val useful = allstates.intersect(accessible(initials,transitions).intersect(coaccessible(finals,transitions)))
+    val allstates = initials ++ finals ++ transitions.flatMap(tr => Seq(tr._1, tr._3)).toSet
+    val useful = allstates.intersect(accessible(initials, transitions).intersect(coaccessible(finals, transitions)))
     val newInit = initials intersect useful
     val newFinals = finals intersect useful
-    var newTransitions : Array[(Int, L, Int)] = Array()
-    for(i<-transitions.indices)
-    {
-      if(useful.contains(transitions(i)._1) && useful.contains(transitions(i)._3))
-      {
-        newTransitions = newTransitions.appended(transitions(i))
-      }
-    }
-  (newInit,newFinals, newTransitions)
+    val newTrans = transitions.flatMap(tr => if (ftrans(useful, tr)) {Set(tr)} else {Set()})
+    (newInit, newFinals, newTrans)
   }
-
  def fchar(c1 : Char, c2 : Char) : Boolean =
   {
     !(c1<c2)
@@ -82,8 +80,9 @@ object Nfa {
   def canonicalize[L](initials : Set[Int], finals: Set[Int],transitions : Array[(Int,L,Int)], f: (L,L)=> Boolean)
   : (Set[Int], Set[Int], Array[(Int, L, Int)]) = {
     if (initials.size != 1) {
-      return (initials, finals, transitions)
+      (initials, finals, transitions)
     }
+    else{
     var count = 1
     var a: (Int, Queue[Int]) = (0, Queue())
     var queue: Queue[Int] = Queue()
@@ -103,13 +102,12 @@ object Nfa {
         }
       }
 
-      for(i<-Range(0,myarray.length-1))
-      {
-       if(!f(myarray(i)._2,myarray(i+1)._2)) {
-          val temp = myarray(i+1)
-          myarray(i+1)=myarray(i)
-         myarray(i)= temp
-       }
+      for (i <- Range(0, myarray.length - 1)) {
+        if (!f(myarray(i)._2, myarray(i + 1)._2)) {
+          val temp = myarray(i + 1)
+          myarray(i + 1) = myarray(i)
+          myarray(i) = temp
+        }
       }
 
       for (i <- myarray.indices) {
@@ -130,6 +128,7 @@ object Nfa {
       }
     }
     (initials.map(mymap), finals.map(mymap), newTransitions)
+  }
   }
 
 /*
@@ -155,13 +154,13 @@ object Nfa {
 
     val myNFAInit= Set(2)
     val myNFAFinal=Set(4,6)
-    val myNFATransitions = Array((2, 'a', 3), (2,'c',5) ,(3, 'b',4), (3, 'b', 6),(5,'b',6),(7,'a',6))
-    /*val (a,b,c) = canonicalize(myNFAInit,myNFAFinal,myNFATransitions,fchar)
+    val myNFATransitions = Array((2, 'a', 3), (5,'c',5) ,(3, 'b',4), (3, 'b', 6),(5,'b',6),(7,'a',6))
+    val (a,b,c) = remove(myNFAInit,myNFAFinal,myNFATransitions)
     println(a)
     println(b)
     for(i<-c.indices){
       println(c(i))
-    }
+    }/*
     val myNFAInit2= Set(12,13)
     val myNFAFinal2=Set(14,16)
     val myNFATransitions2 = Array((12, 'a', 13), (14, 'b', 16), (13, 'b', 16))
