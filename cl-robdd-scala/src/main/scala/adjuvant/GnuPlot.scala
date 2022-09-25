2a36adf7f1d598b08db4d3e62bd81dfef93f8970
Jim Newton <jnewton@lrde.epita.fr>
2022-09-25 11:45:00 +0200

    refactored to relax input data format

--- cl-robdd-scala/src/main/scala/adjuvant/GnuPlot.scala
+++ cl-robdd-scala/src/main/scala/adjuvant/GnuPlot.scala
@@ -44,7 +44,120 @@ object GnuPlot {
     if (verbose)
       println(s"finished $csvName]")
   }
+  case class GnuPlotDescriptor(terminals: Set[String] = Set("png"), // e.g Set("png","tikz")
+                               title: String = "",
+                               comment: String = "",
+                               xAxisLabel: String = "",
+                               xLog: Boolean = false,
+                               yAxisLabel: String = "",
+                               yLog: Boolean = false,
+                               grid: Boolean = false,
+                               // outputDirName     : String = "/tmp/",
+                               // outputFileBaseName is basename without the .pdf, .gnu, .png etc and without leading path
+                               outputFileBaseName: String = "curves",
+                               plotWith: String = "linespoints",
+                               key: String = "horizontal bmargin",
+                               gnuFileCB: String => Unit = (_) => (),
+                               verbose: Boolean,
+                               view: Boolean = false) {
+    def plot(dataToPlot: Seq[(String, Seq[Double], Seq[Double])]): Unit = {
 
+      // TODO verify that "lines" can be used as plotWith, not 100% sure
+      require(Set("linespoints", "points", "lines").contains(plotWith))
+
+      import adjuvant.Accumulators.withOutputToString
+      import adjuvant.Adjuvant.openGraphicalFile
+
+      import java.io._
+
+      val gnuName = makeTmpFileName(outputFileBaseName, ".gnu")
+      val gnu = new PrintWriter(new File(gnuName))
+
+      writeCsv(dataToPlot, xAxisLabel, outputFileBaseName, verbose)
+      if (verbose)
+        println(s"[writing to $gnuName\n")
+      gnu.write(s"# $comment\n")
+
+      def logCompatible(projection: ((String, Seq[Double], Seq[Double])) => Seq[Double]): Boolean = {
+        dataToPlot.forall { data =>
+          val numbers = projection(data)
+          numbers.forall(_ > 0) && numbers.nonEmpty && (numbers.max > numbers.min)
+        }
+      }
+      // turn off log scale if range on axis is 0 or if it contains 0 or negative values
+      if (xLog && logCompatible(_._2))
+        gnu.write("set logscale x\n")
+      if (yLog && logCompatible(_._3))
+        gnu.write("set logscale y\n")
+      if ("" != xAxisLabel)
+        gnu.write(s"""set xlabel "$xAxisLabel" font ",15"\n""")
+      if ("" != yAxisLabel)
+        gnu.write(s"""set ylabel "$yAxisLabel" font ",15"\n""")
+      if (grid)
+        gnu.write(s"set grid\n")
+      gnu.write("set key font ',15'\n")
+      gnu.write("set xtics font ',15'\n")
+      gnu.write("set ytics font ',15'\n")
+      gnu.write(s"set key $key\n") // TODO can also use set key at x,y
+      if ("" != title)
+        gnu.write(s"""set title "$title"\n""")
+      gnu.write("plot ")
+      val footer: String = withOutputToString { prFooter =>
+        val header: String = withOutputToString { prHeader =>
+          def plot(title: String, xys: Seq[(Double, Double)]): Unit = {
+            prHeader(""""-" using 1:2""")
+            prHeader(s" with $plotWith")
+            prHeader(s""" title "$title"""")
+            prFooter(s"""#$title\n""")
+            xys.foreach { case (x, y) =>
+              prFooter(s"$x $y\n")
+            }
+            prFooter("end\n")
+          }
+
+          if (dataToPlot.nonEmpty) {
+            plot(dataToPlot.head._1, dataToPlot.head._2.zip(dataToPlot.head._3))
+
+            dataToPlot.tail.foreach { case (curveTitle, xs, ys) =>
+              prHeader(",\\\n    ")
+              plot(curveTitle, xs.zip(ys))
+            }
+          }
+        }
+        gnu.write(s"$header\n")
+      }
+      gnu.write(footer)
+      gnu.close()
+      gnuFileCB(gnuName)
+      if (verbose)
+        println(s"finished $gnuName]")
+
+      if (dataToPlot.exists { data: (String, Seq[Double], Seq[Double]) =>
+        data._2.nonEmpty && data._3.nonEmpty
+      })
+        terminals.foreach { terminal =>
+          import sys.process._
+          val outputFileName = makeTmpFileName(outputFileBaseName, terminal)
+
+          if (verbose)
+            println(s"[generating $outputFileName")
+          gnu.write("\n")
+          val process = Process(Seq(gnuPlotPath, "-e", s"set terminal $terminal", gnuName),
+                                None,
+                                // The LC_CTYPE env var prevents the following diagnostic from gnuplot
+                                // Fontconfig warning: ignoring UTF-8: not a valid region tag
+                                "LC_CTYPE" -> "en_US.UTF-8")
+          val exitCode = (process #>> new File(outputFileName)).!
+
+          if (exitCode != 0)
+            println(s"finished $outputFileName with exit code=$exitCode verbose=$verbose]")
+          else if (verbose)
+            println(s"finished $outputFileName]")
+          if (exitCode == 0 && view)
+            openGraphicalFile(outputFileName)
+        }
+    }
+  }
   // Create a plot using gnuplot (which we assume is stalled
   //  on the system).
   //   dataToPlot specifies a sequence of *curves*:
@@ -68,7 +181,10 @@ object GnuPlot {
   //       this comment does not have any effect on the plot, but may be
   //       used to distinguish the .gnu file as necessary.
   //  title: optional title to display on the plot file.
-  def gnuPlot(dataToPlot: Seq[(String, Seq[Double], Seq[Double])])(
+  def gnuPlot[A](dataToPlot: Seq[A]
+                 // dataToPlot: Seq[(String, Seq[Double], Seq[Double])]
+                 // dataToPlot: Seq[(String, Seq[(Double,Double)])]
+             )(
     terminals: Set[String] = Set("png"), // e.g Set("png","tikz")
     title: String = "",
     comment: String = "",
@@ -83,102 +199,38 @@ object GnuPlot {
     plotWith: String = "linespoints",
     key: String = "horizontal bmargin",
     gnuFileCB: String => Unit = (_) => (),
-    verbose: Boolean,
+    verbose: Boolean = false,
     view: Boolean = false
   ): Unit = {
-    // TODO verify that "lines" can be used as plotWith, not 100% sure
-    require(Set("linespoints", "points", "lines").contains(plotWith))
-
-    import adjuvant.Accumulators.withOutputToString
-    import adjuvant.Adjuvant.openGraphicalFile
-
-    import java.io._
-
-    val gnuName = makeTmpFileName(outputFileBaseName, ".gnu")
-    val gnu = new PrintWriter(new File(gnuName))
-
-    writeCsv(dataToPlot, xAxisLabel, outputFileBaseName, verbose)
-    if (verbose)
-      println(s"[writing to $gnuName\n")
-    gnu.write(s"# $comment\n")
-
-    def logCompatible(projection: ((String, Seq[Double], Seq[Double])) => Seq[Double]): Boolean = {
-      dataToPlot.forall { data =>
-        val numbers = projection(data)
-        numbers.forall(_ > 0) && numbers.nonEmpty && (numbers.max > numbers.min)
-      }
+    val gpd = GnuPlotDescriptor(terminals = terminals,
+                                title = title,
+                                comment = comment,
+                                xAxisLabel = xAxisLabel,
+                                xLog = xLog,
+                                yAxisLabel = yAxisLabel,
+                                yLog = yLog,
+                                grid = grid,
+                                outputFileBaseName = outputFileBaseName,
+                                plotWith = plotWith,
+                                key = key,
+                                gnuFileCB = gnuFileCB,
+                                verbose = verbose,
+                                view = view)
+    val curves = dataToPlot.map{
+      case (label:String,xys:Seq[(Double,Double)]) => (label,xys.map(_._1),xys.map(_._2))
+      case (label:String,xs:Seq[Double],ys:Seq[Double]) => (label,xs,ys)
     }
-    // turn off log scale if range on axis is 0 or if it contains 0 or negative values
-    if (xLog && logCompatible(_._2))
-      gnu.write("set logscale x\n")
-    if (yLog && logCompatible(_._3))
-      gnu.write("set logscale y\n")
-    if ("" != xAxisLabel)
-      gnu.write(s"""set xlabel "$xAxisLabel" font ",15"\n""")
-    if ("" != yAxisLabel)
-      gnu.write(s"""set ylabel "$yAxisLabel" font ",15"\n""")
-    if (grid)
-      gnu.write(s"set grid\n")
-    gnu.write("set key font ',15'\n")
-    gnu.write("set xtics font ',15'\n")
-    gnu.write("set ytics font ',15'\n")
-    gnu.write(s"set key $key\n") // TODO can also use set key at x,y
-    if ("" != title)
-      gnu.write(s"""set title "$title"\n""")
-    gnu.write("plot ")
-    val footer: String = withOutputToString { prFooter =>
-      val header: String = withOutputToString { prHeader =>
-        def plot(title: String, xys: Seq[(Double, Double)]): Unit = {
-          prHeader(""""-" using 1:2""")
-          prHeader(s" with $plotWith")
-          prHeader(s""" title "$title"""")
-          prFooter(s"""#$title\n""")
-          xys.foreach { case (x, y) =>
-            prFooter(s"$x $y\n")
-          }
-          prFooter("end\n")
-        }
+    gpd.plot(dataToPlot=curves)
+  }
 
-        if (dataToPlot.nonEmpty) {
-          plot(dataToPlot.head._1, dataToPlot.head._2.zip(dataToPlot.head._3))
-
-          dataToPlot.tail.foreach { case (curveTitle, xs, ys) =>
-            prHeader(",\\\n    ")
-            plot(curveTitle, xs.zip(ys))
-          }
-        }
-      }
-      gnu.write(s"$header\n")
-    }
-    gnu.write(footer)
-    gnu.close()
-    gnuFileCB(gnuName)
-    if (verbose)
-      println(s"finished $gnuName]")
-
-    if (dataToPlot.exists { data: (String, Seq[Double], Seq[Double]) =>
-      data._2.nonEmpty && data._3.nonEmpty
-    })
-      terminals.foreach { terminal =>
-        import sys.process._
-        val outputFileName = makeTmpFileName(outputFileBaseName, terminal)
-
-        if (verbose)
-          println(s"[generating $outputFileName")
-        gnu.write("\n")
-        val process = Process(Seq(gnuPlotPath, "-e", s"set terminal $terminal", gnuName),
-                              None,
-                              // The LC_CTYPE env var prevents the following diagnostic from gnuplot
-                              // Fontconfig warning: ignoring UTF-8: not a valid region tag
-                              "LC_CTYPE" -> "en_US.UTF-8")
-        val exitCode = (process #>> new File(outputFileName)).!
-
-        if (exitCode != 0)
-          println(s"finished $outputFileName with exit code=$exitCode verbose=$verbose]")
-        else if (verbose)
-          println(s"finished $outputFileName]")
-        if (exitCode == 0 && view)
-          openGraphicalFile(outputFileName)
-      }
+  def main(argv:Array[String]):Unit = {
+    gnuPlot(dataToPlot=Seq(("curve1",
+                             Seq(1.0,2.0,3.0,4),
+                             Seq(1.0,2.0, 2.5, 2.75)),
+                           ("curve2", Seq((1.0,2.0),
+                                          (2.0,2.25),
+                                          (3.0,2.125),
+                                          (4.0,2.012)))))(
+            view=true)
   }
 }
