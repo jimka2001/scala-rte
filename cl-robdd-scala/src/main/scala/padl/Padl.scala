d12d68274a2c89e2f5e8f3342b44387459fbb756
Jim Newton <jnewton@lrde.epita.fr>
2022-09-26 14:13:12 +0200

    added padl-2023 example code

---
+++ cl-robdd-scala/src/main/scala/padl/Padl.scala
@@ -0,0 +1,48 @@
+// Copyright (©) 2023 EPITA Research and Development Laboratory
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package padl
+
+import genus.SAtomic
+import genus.Types.{evenType, oddType}
+import rte.{Cat, Or, Rte, Singleton, Star}
+import xymbolyco.GraphViz
+import xymbolyco.GraphViz.dfaView
+
+
+object Padl {
+
+  def example1():Unit = {
+    val str = Singleton(SAtomic(classOf[String]))
+    val odd = Singleton(oddType)
+    val even = Singleton(evenType)
+    val integer = Singleton(SAtomic(classOf[Int]))
+    val rt1: Rte = Cat(str, Or(Cat(odd, Star(integer)),
+                               Cat(odd, str)))
+
+    dfaView(rt1.toDfa(), abbrev = true, title = "rt1", showSink=false)
+  }
+
+  def main(argv: Array[String]): Unit = {
+    example1()
+  }
+
+}
