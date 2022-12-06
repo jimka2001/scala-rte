ff95b92011efc7f90f653ec74e28775ba2b7f1e3
Axel Gautier <ax.gautier@yahoo.com>
2022-12-06 15:19:57 +0100

    the random type generator works as intended and passes every test

--- cl-robdd-scala/src/test/scala/xymbolyco/KnownBugsSuite.scala
+++ cl-robdd-scala/src/test/scala/xymbolyco/KnownBugsSuite.scala
@@ -68,5 +68,34 @@ class KnownBugsSuite extends AnyFunSuite {
     val dfa = Minimize.minimize(Minimize.trim(rte.toDfa(42)))
     assert(dfa.Qids.size == 4)
   }*/
+
+  test("type A") {
+    val data = for {depth <- Range(0, 3)
+                    _ <- Range(0, 10000)}
+      yield RandomType.randomType(depth, Some(false)).inhabited
+    assert(data.contains(None) && data.contains(Some(true)) && !data.contains(Some(false)))
+  }
+  test("type B") {
+    val data = for {depth <- Range(0, 3)
+                    _ <- Range(0, 10000)}
+      yield RandomType.randomType(depth, Some(true)).inhabited
+    assert(data.contains(None) && data.contains(Some(true)) && data.contains(Some(false)))
+  }
+  test("type C") {
+    SAtomic.withOpenWorldView {
+      val data = for {depth <- Range(0, 3)
+                      _ <- Range(0, 10000)}
+        yield RandomType.randomType(depth, Some(false)).inhabited
+      assert(data.contains(None) && data.contains(Some(true)) && !data.contains(Some(false)))
+    }
+  }
+  test("type D") {
+    SAtomic.withOpenWorldView {
+      val data = for {depth <- Range(0, 3)
+                      _ <- Range(0, 10000)}
+        yield RandomType.randomType(depth, Some(true)).inhabited
+      assert(data.contains(None) && data.contains(Some(true)) && data.contains(Some(false)))
+    }
+  }
 }
 
