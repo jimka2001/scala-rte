0087938429bf397e6e4056fd1e706cb55c68c210
Theo Gardet <theo.gardet@epita.fr>
2023-03-31 14:42:05 +0200

    feat(generator): working genus generator

--- cl-robdd-scala/src/test/scala/genus/GenusPropertyBasedTests.scala
+++ cl-robdd-scala/src/test/scala/genus/GenusPropertyBasedTests.scala
@@ -1,9 +1,15 @@
 package genus
 
+import org.scalacheck.Gen
 import genus.SimpleTypeD
 import genus.GenusSpecifications
 
 object GenusPropertyBasedTests extends App {
+  // TODO: Create arbitrary generator for trees => properties
   val genusSample = GenusSpecifications.naiveGenGenus.sample.get
-  val genusSample.toDot()
+  println(genusSample)
+  val dot = genusSample.toDot()
+  // println(System.getProperty("user.dir") + "/src/test/scala/genus/dot/naiveGenGenus.dot")
+  // TODO: Write dot in a file
+  // TOOD: display dot
 }
--- cl-robdd-scala/src/test/scala/genus/GenusSpecifications.scala
+++ cl-robdd-scala/src/test/scala/genus/GenusSpecifications.scala
@@ -4,46 +4,62 @@ import org.scalacheck.rng.Seed
 import org.scalacheck.{Arbitrary, Cogen, Gen}
 
 object GenusSpecifications {
-  def naiveGenGenus: Gen[SimpleTypeD] = {
-    def genInternalNode(left: SimpleTypeD, right: SimpleTypeD) = Gen.frequency(
-      (1, SNot(right)), // Arbitrary choice of right
-      (1, SAnd(left, right)),
-      (1, SOr(left, right))
-    )
+  def naiveGenGenus: Gen[SimpleTypeD] = Gen.lzy { Gen.sized { size =>
 
-    def genLeaf() =  {
-      // FIXME: Fix predicate generator
-//      def createPredicate[Any, Boolean](seed0: Seed, cogen: Cogen[Any], gen: Gen[Boolean]): Any => Boolean =
-//        n => {
-//          val seed1 = cogen.perturb(seed0, n)
-//          gen.run(seed1)._1
-//        }
+      lazy val genLeaf = {
+        //      def createPredicate[Any, Boolean](seed0: Seed, cogen: Cogen[Any], gen: Gen[Boolean]): Any => Boolean =
+        //        n => {
+        //          val seed1 = cogen.perturb(seed0, n)
+        //          gen.run(seed1)._1
+        //        }
 
-      def genPredicate: Gen[AnyVal => Boolean] = {
-        def predicate(a: AnyVal, b: Boolean): AnyVal => Boolean = ((a: AnyVal) => b)
-        for {
-          anyval <- Arbitrary.arbitrary[AnyVal]
-          bool <- Arbitrary.arbitrary[Boolean]
-        } yield predicate(anyval, bool)
+        // def genPredicate: Gen[AnyVal => Boolean] = {
+        //   def predicate(a: AnyVal, b: Boolean): AnyVal => Boolean = ((a: AnyVal) => b)
+
+        //   for {
+        //     anyval <- Arbitrary.arbitrary[AnyVal]
+        //     bool <- Arbitrary.arbitrary[Boolean]
+        //   } yield predicate(anyval, bool)
+        // }
+
+        // implicit lazy val arbPredicate = Arbitrary(genPredicate)
+
+        // TODO: Generate predicates to be able to generate SAtomic types
+        // TODO: Generate SMembers
+        Gen.frequency(
+          (1, SAtomic(Arbitrary.arbitrary[AnyVal].getClass)),
+          (1, SEql(Arbitrary.arbitrary[AnyVal])),
+          (1, SEql(Arbitrary.arbitrary[AnyVal])),
+          // (1, SMember(Gen.listOf[Any])), // <- smol aled
+          // (1, SSatisfies(arbPredicate)) // <- Aled
+          (1, STop),
+          (1, SEmpty),
+        )
       }
 
-      implicit lazy val arbPredicate = Arbitrary(genPredicate)
+      lazy val genInternalNode = {
+        lazy val genAnd = for {
+          left <- naiveGenGenus
+          right <- naiveGenGenus
+        } yield SAnd(left, right)
 
-      Gen.frequency(
-        (1, SAtomic(Arbitrary.arbitrary[AnyVal].getClass)),
-        (1, SEql(Arbitrary.arbitrary[AnyVal])),
-        (1, SEql(Arbitrary.arbitrary[AnyVal])),
-        (1, SMember(Gen.listOf[Any])),
-        (1, SSatisfies(arbPredicate)) // <- Aled
-        (1, STop),
-        (1, SEmpty),
-      )
+        lazy val genOr = for {
+          left <- naiveGenGenus
+          right <- naiveGenGenus
+        } yield SOr(left, right)
+
+        lazy val genNot = for {
+          arg <- naiveGenGenus
+        } yield SNot(arg)
+
+        Gen.frequency(
+          1 -> Gen.lzy(genAnd),
+          1 -> Gen.lzy(genOr),
+          1 -> Gen.lzy(genNot)
+        )
+      }
+
+      Gen.oneOf(genInternalNode, genLeaf)
     }
-
-    for {
-      left <- naiveGenGenus
-      right <- naiveGenGenus
-      v <- Gen.oneOf(genInternalNode(left, right), genLeaf())
-    } yield v
   }
 }
