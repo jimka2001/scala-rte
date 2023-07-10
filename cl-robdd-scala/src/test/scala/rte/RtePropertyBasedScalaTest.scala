41dbc8fa453fc7f8c5e902717b4eef49661a4362
Theo Gardet <theo.gardet@epita.fr>
2023-07-10 16:25:11 +0200

    fix: boltzmann generator (generating functions, samplers) now corresponds to the grammar, removed changes made in xymbolico, proper classification main

--- cl-robdd-scala/src/test/scala/rte/RtePropertyBasedScalaTest.scala
+++ cl-robdd-scala/src/test/scala/rte/RtePropertyBasedScalaTest.scala
@@ -9,53 +9,73 @@ import org.scalatest.prop.Configuration
 import rte.RteSpecifications.naiveGenRte
 import xymbolyco.mystats.{brz, brzmin, thmp, thmpmin}
 import xymbolyco.{GraphViz, RTEStatistics, mystats}
+
+import java.lang.System.console
 import scala.collection.immutable.{Map, Set}
-
-
 import scala.Predef.->
 
 object RtePropertyBasedScalaTest extends App {
-    // This is a copy of the statisticSizePerfDFAfromF in the xymbolico package, adapted to work with a generator.
+  def classifyRte(n: Int, size: Int, stdDepth: Int)= {
+    val boltzmann = new RteBoltzmann(size, stdDepth)
+    implicit lazy val arbitraryGen: Arbitrary[Rte] = Arbitrary(boltzmann.RteBoltzmannGen())
 
-    mystats.statisticSizePerDFAfromGen(5, 5, 10, 1, naiveGenRte(5))
-//  val rteList = for {
-//    _ <- 0 until 5
-//  } yield naiveGenRte(25).sample.get
-//  for (g <- rteList) {
-//    println(g.toLaTeX())
-//    // FIXME: plot the dfa ( see below v )
-//    GraphViz.dfaView(g.toDfa(),abbrev=true,title="rt1")
-//  }
-}
 
-// Standalone PBT tests
-object RteSpecification extends Properties("Rte") {
-  val size = 25
-  val botlzmann = new RteBoltzmann(size)
-  implicit lazy val arbitraryGen: Arbitrary[Rte] = Arbitrary(botlzmann.RteBoltzmannGen())
+    // Classification of the generated output
+    var empty = 0
+    var satisfiable = 0
+    var indeterminate = 0
 
-  override def overrideParameters(p: Test.Parameters): Test.Parameters = p.withMinSuccessfulTests(10000)
+    for {
+      i <- Range(0, n)
+    } {
+      val rte = boltzmann.RteBoltzmannGen().sample.get
+      val dfa = rte.toDfa(42)
 
-  // Test for classifying the type of input. The input will be generated for all properties, so it is not a fact about the repartition of the input, rather a hint
-  property("test") = forAll { (r: Rte) =>
-    // FIXME: stats about rte generation (See xymbolyco/RteStatistics)
-    val dfa = r.toDfa()
-//    print(dfa.toString)
-    true
+      if (dfa.vacuous().getOrElse(false))
+        empty += 1
+      else dfa.spanningPath match {
+        case Some(Left(_)) => indeterminate += 1
+        case Some(Right(_)) => satisfiable += 1
+      }
+    }
+
+    empty = empty * 100 / n
+    satisfiable = satisfiable * 100 / n
+    indeterminate = indeterminate * 100 / n
+
+    Console.println(s"classification of ${n} RTEs of approximate size ${size}")
+    Console.println(s"empty=${empty}%")
+    Console.println(s"satisfiable=${satisfiable}%")
+    Console.println(s"indeterminate=${indeterminate}%")
+    Console.println(s"total=${indeterminate + empty + satisfiable}% (the difference between the total and 100% is due to floating point imprecisions")
   }
-}
 
-// PBT linked with the testsuite
-class RtePBT extends MyFunSuite with Matchers with Configuration {
-  val depth = 10
-  implicit override val generatorDrivenConfig = PropertyCheckConfiguration(minSuccessful = 100000, workers = 4)
-  implicit lazy val arbitraryGen: Arbitrary[Rte] = Arbitrary(naiveGenRte(depth))
+  def viewRte(n: Int, size: Int, stdDepth: Int) = {
+    val boltzmann = new RteBoltzmann(size, stdDepth)
+    implicit lazy val arbitraryGen: Arbitrary[Rte] = Arbitrary(boltzmann.RteBoltzmannGen())
 
-  test("test") {
-    forAll { (r: Rte) =>
-      val dfa = r.toDfa()
-//      print(dfa.toString)
-      true
+    for {
+      i <- Range(0, n)
+    } {
+      val rte = boltzmann.RteBoltzmannGen().sample.get
+      val dfa = rte.toDfa(42)
+      GraphViz.dfaView(dfa)
     }
   }
+
+  def printRte(n: Int, size: Int, stdDepth: Int) = {
+    val boltzmann = new RteBoltzmann(size, stdDepth)
+    implicit lazy val arbitraryGen: Arbitrary[Rte] = Arbitrary(boltzmann.RteBoltzmannGen())
+
+    for {
+      i <- Range(0, n)
+    } {
+      val rte = boltzmann.RteBoltzmannGen().sample.get
+      Console.println(rte.toMachineReadable())
+    }
+  }
+
+  viewRte(10, 25, 1)
+//  printRte(100, 25, 1)
+//  classifyRte(100, 25, 1)
 }
