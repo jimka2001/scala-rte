b3c4f8a156a74906184c1b85ca19631b63e1627b
Jim Newton <jnewton@lrde.epita.fr>
2024-02-28 15:44:36 +0100

    added Relations examples

---
+++ cl-robdd-scala/src/main/scala/forcomprehension/Relations.scala
@@ -0,0 +1,45 @@
+package forcomprehension
+import HeavyBool._
+
+object Relations {
+  def isReflexive[T](gen:LazyList[T], rel:(T,T)=>Boolean) = {
+    forallM[T](gen, (x:T) =>
+      forallM[T]( gen, (y:T) => HeavyBool(rel(x,y),
+                                          List(Map("x" -> x, "y" -> y)))))
+      .conjTrue(Map("reason" -> "reflexive"))
+      .conjFalse(Map("reason" -> "not reflexive"))
+  }
+
+  def isSymmetric[T](gen:LazyList[T], rel:(T,T)=>Boolean) = {
+    def hrel(a:T, b:T) = HeavyBool(rel(a,b), List())
+    forallM[T](gen, (x:T) =>
+      forallM[T]( gen, (y:T) => hrel(x,y) ==> hrel(y,x)))
+      .conjTrue(Map("reason" -> "symmetric"))
+      .conjFalse(Map("reason" -> "not symmetric"))
+  }
+
+  def isTransitive[T](gen:LazyList[T], rel:(T,T)=>Boolean):HeavyBool = {
+    def hrel(a:T, b:T) = HeavyBool(rel(a,b), List())
+    forallM[T](gen, (x:T) =>
+               forallM[T](gen, (y:T) => {
+                 if (rel(x,y))
+                   forallM[T](gen, (z:T) =>
+                     (hrel(y,z) ==> hrel(x,z)))
+                 else
+                   HTrue
+               }))
+      .conjTrue(Map("reason" -> "transitive"))
+      .conjFalse(Map("reason" -> "not transitive"))
+  }
+
+  def isEquivalence[T](gen:LazyList[T], rel:(T,T)=>Boolean):HeavyBool = {
+    (isSymmetric(gen,rel) && isReflexive(gen,rel) && isTransitive(gen,rel))
+      .conjTrue(Map("reason" -> "equivalence"))
+      .conjFalse(Map("reason" -> "not equivalence"))
+  }
+
+  def main(argv:Array[String]):Unit = {
+    println(forallM(LazyList.range(1,20), (a:Int) => HeavyBool(a < 12, List())))
+    println(isEquivalence(LazyList.range(1,20), (a:Int, b:Int) => a != b))
+  }
+}
