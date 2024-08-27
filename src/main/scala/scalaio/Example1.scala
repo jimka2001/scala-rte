7104ee803f6d75dd5aeb81bcfbdbf29ad1ea6ac5
Jim Newton <jnewton@lrde.epita.fr>
2024-08-27 12:24:34 +0200

    using syntax sugar to simplify slides

---
+++ src/main/scala/scalaio/Example1.scala
@@ -0,0 +1,25 @@
+package scalaio
+
+import genus.SAtomic
+import rte.{Atomic, Eql, Rte}
+import rte.RteImplicits.classToRte
+
+object Example1 {
+  val data = Seq("C", 100, 200, 300,
+                 "M", 10.0, 20.0,
+                 "M",
+                 "C", 1, 2, 3,
+                 "C",-1, -3, -7, -8)
+
+  val F:Rte = Atomic(classOf[Double]) | Atomic(classOf[Float])
+  val I:Rte = Atomic(classOf[Int])
+  val keyM:Rte = Eql("M")
+  val keyC:Rte = Eql("C")
+
+  val re:Rte = ((keyC ++ I.*) | (keyM ++ F.*)).*
+  println(re.contains(data))
+
+  def main(argv:Array[String]):Unit = {
+
+  }
+}
