f38451e104d6c34450094d2f0cf0b9a0dd60ab29
Jim Newton <jnewton@lrde.epita.fr>
2024-11-02 16:00:39 +0100

    improving demo for live use

--- src/main/scala/scalaio/Demo1.scala
+++ src/main/scala/scalaio/Demo1.scala
@@ -4,10 +4,10 @@ import rte.{Rte, Xor, Atomic, Satisfies, Eql}
 import xymbolyco.GraphViz.dfaView
 
 object Demo1 {
-  val data:Seq[Any] = Seq("M", 0.1, 0.3, 4.5, // "M" designates measurements, 1 or more double or float
+  val data:Seq[Any] = Seq("M", 0.1, 0.3F, 4.5, // "M" designates measurements, 1 or more double or float
                           "C", 1, 5, 7, 8, // "C" designates counts, 1 or more ints, all positive
                           "C", 2, 5, 3,
-                          "M", 0.5, 1.2
+                          "M", 0.5, 1.2F
                           )
   val I: Rte = Atomic(classOf[Int])
   val DF:Rte = Atomic(classOf[Float]) | Atomic(classOf[Double])
@@ -28,24 +28,27 @@ object Demo1 {
 
   def main(argv:Array[String]):Unit = {
     val pattern1:Rte = ( Mclause | Cclause).*
-    println("Pattern 1 contains data1? --> " + pattern1.contains(data))
+    val m1 = pattern1.contains(data)
+    println("Pattern 1 contains data1? --> " + m1)
     dfaView(pattern1.toDfa(), title="Pattern 1", showSink=false, abbrev=true)
 
     val pattern2:Rte = ((M ++ DF.+) | (C ++ I.+)).*
-    println("Pattern 2 contains data1? --> " + pattern2.contains(data))
+    val m2 = pattern2.contains(data)
+    println("Pattern 2 contains data1? --> " + m2)
     dfaView(pattern2.toDfa(), title="Pattern 2", showSink=false, abbrev=true)
-
-    println()
-
+    
     val pattern_xor:Rte = Xor(pattern1, pattern2)
     val diff_dfa = pattern_xor.toDfa()
     dfaView(diff_dfa, title="Symmetric Difference", showSink=false, abbrev=true)
 
-    println("Spanning types: --> " + diff_dfa.spanningTrace)
-    println("Spanning trace: --> " + diff_dfa.witness)
-    println()
+    val trace = diff_dfa.spanningTrace
+    val witness = diff_dfa.witness
+    println("Spanning types: --> " + trace)
+    println("Spanning trace: --> " + witness)
 
-    println("Pattern 1 contains? --> " + pattern1.contains(Seq("C", 0)))
-    println("Pattern 2 contains? --> " + pattern2.contains(Seq("C", 0)))
+    val m1a = pattern1.contains(Seq("C", 0))
+    val m2a = pattern2.contains(Seq("C", 0))
+    println("Pattern 1 contains? --> " + m1a)
+    println("Pattern 2 contains? --> " + m2a)
   }
 }
