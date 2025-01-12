15f4df1da733d37fbf0632eb9948ebdebdf0d727
Jim Newton <jimka.issy@gmail.com>
2025-01-12 14:02:25 +0100

    updated demo

--- src/main/scala/demos/padl2025/Demo.scala
+++ src/main/scala/demos/padl2025/Demo.scala
@@ -2,56 +2,84 @@ package demos.padl2025
 
 import rte._
 import xymbolyco.GraphViz.dfaView
+import xymbolyco.{Satisfiable,Indeterminate}
 
 object Demo {
 
-  val data:Seq[Any] = Seq("Lorem", "ipsum", 1, 2, 3, "dolor", 10, 20)
-  val S:Rte = Atomic(classOf[String])
-  val I:Rte = Atomic(classOf[Int])
-
-  def positive(x:Any):Boolean = {
-    x match {
-      case a:Int => a > 0
+  def large(n:Any):Boolean = {
+    n match {
+      case n: Int => n > 128
       case _ => false
     }
   }
 
-  val Pos:Rte = Satisfies(positive, "Pos")
+  def even(n:Any):Boolean = {
+    n match {
+      case n: Int => n % 2 == 0
+      case _ => false
+    }
+  }
 
+  val data:Seq[Any] = Seq("Lorem", "ipsum", 2, 4, 8, "dolor", 10, 20)
 
-  val pattern1:Rte = S ++ (S | I).+
-  val pattern2:Rte = S ++ I.* ++ (S | I & Pos).+
+  val S:Rte = Atomic(classOf[String])
+  val I:Rte = Atomic(classOf[Int])
+  val Even:Rte = Satisfies(even, "Even")
+  val Large:Rte = Satisfies(large, "Large")
 
+  val pattern1:Rte = (S ++ Even.*).*
+  val pattern2:Rte = (S ++ (I & Even).*).*
+  val pattern3:Rte = (S ++ (I & Even & Large).*).*
 
   def main(argv:Array[String]):Unit = {
 
-    val m1 = pattern1.contains(data, verbose=false)
+    val m1 = pattern1.contains(data)
     val m2 = pattern2.contains(data)
+    val m3 = pattern3.contains(data)
 
-    println("Pattern 1 contains data? --> " + m1)
-    println("Pattern 2 contains data? --> " + m2)
+    println("Pattern 1 matches data? --> " + m1)
+    println("Pattern 2 matches data? --> " + m2)
+    println("Pattern 3 matches data? --> " + m3)
 
     dfaView(pattern1.toDfa(), title="Pattern 1", abbrev=true)
     dfaView(pattern2.toDfa(), title="Pattern 2", abbrev=true)
+    dfaView(pattern3.toDfa(), title="Pattern 3", abbrev=true)
 
-    val pattern_xor:Rte = Xor(pattern1, pattern2)
-    val diff_dfa = pattern_xor.toDfa()
-    dfaView(diff_dfa, title="Symmetric Difference", abbrev=true)
+    val pairs = List((pattern1, "P1"),
+                     (pattern2, "P2"),
+                     (pattern3, "P3"))
+    for{ (p,pl) <- pairs
+         (q,ql) <- pairs
+         if p != q
+         diff = (p - q).toDfa()
+         label = s"Query $pl - $ql"
+         } {
+      println("--------------------")
+      println(s"$pl = $p")
+      println(s"$ql = $q")
+      dfaView(diff, title=label, abbrev=true)
+      println(label)
+
+      diff.spanningTrace match {
+        case None =>
+          println(s"   Subtype: $pl < $ql")
+          println(s"   All Elements in $ql are in $ql")
+          println("   Spanning types: --> " + diff.spanningTrace)
+        case Some((Satisfiable,_)) =>
+          println(s"   NOT Subtype: $pl !< $ql")
+          println(s"   Elements in $pl and not in $ql")
+          println("   Spanning types: --> " + diff.spanningTrace)
+        case Some((Indeterminate, _)) =>
+          println(s" DONT KNOW Subtype: $pl, $ql")
+          println(s"   MAYBE Elements in $pl and not in $ql")
+          println("   Spanning types: --> " + diff.spanningTrace)
+        case _ => ()
+      }
 
 
+      println("   Spanning trace: --> " + diff.witness)
 
-    // Question: Is pattern1 a subset of pattern2?
-    val isSubset12 = (pattern1 & !pattern2).toDfa()
-    dfaView(isSubset12, title="Subset P1 < P2 Query", abbrev=true)
-    println("P1 < P2 Spanning types: --> " + isSubset12.spanningTrace)
-    println("P1 < P2 Spanning trace: --> " + isSubset12.witness)
-
-
-    // Question: Is pattern1 a subset of pattern2?
-    val isSubset21 = (pattern2 & !pattern1).toDfa()
-    dfaView(isSubset21, title="Subset P2 < P1 Query", abbrev=true)
-    println("P2 < P1 Spanning types: --> " + isSubset21.spanningTrace)
-    println("P2 < P1 Spanning trace: --> " + isSubset21.witness)
+    }
 
   }
 }
