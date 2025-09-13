7199a2d56dd7858d25899bb80f39304e5967624f
Jim Newton <jim.newton@epita.fr>
2025-09-13 09:38:23 +0200

    moved demo file

--- src/main/scala/demos/psug2023/Psug2023.scala
+++ src/main/scala/demos/psug2023/Psug2023.scala
@@ -19,16 +19,16 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-package psug
+package demos.psug2023
 
+import adjuvant.GraphViz.multiLineString
 import genus._
 import rte.Rte.rteIfThenElse
-import rte.{Cat, _}
-import xymbolyco.GraphViz.{dfaView}
-import adjuvant.GraphViz.multiLineString
+import rte._
+import xymbolyco.GraphViz.dfaView
 
 
-object Psug {
+object Psug2023 {
   import rte.RteImplicits._
 
   import scala.language.implicitConversions
