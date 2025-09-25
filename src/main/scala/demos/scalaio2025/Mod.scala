59c2b52ec74864ef773d700d6f9ed1b9b3ad3751
Jim Newton <jim.newton@epita.fr>
2025-09-25 10:45:20 +0200

    refactoring statistics generator

--- src/main/scala/demos/scalaio2025/Mod.scala
+++ src/main/scala/demos/scalaio2025/Mod.scala
@@ -1,11 +1,14 @@
 package demos.scalaio2025
+
 import adjuvant.Adjuvant.randCase
+
 abstract class Expr
 
-case class Plus(a:Expr, b:Expr) extends Expr
-case class Times(a:Expr, b:Expr) extends Expr
-case class Const(n:Int) extends Expr
+case class Plus(a: Expr, b: Expr) extends Expr
 
+case class Times(a: Expr, b: Expr) extends Expr
+
+case class Const(n: Int) extends Expr
 
 
 object Expr {
@@ -19,91 +22,81 @@ object Expr {
     }
   }
 
-  def naiveRand(depth: Int, p:Int): Expr = {
+  def naiveRand(depth: Int, p: Int): Expr = {
     if (depth == 0)
       Const(random.nextInt(p))
     else
       randCase(() => Const(random.nextInt(p)),
-               Seq((0.333, () => Plus(naiveRand(depth-1, p), naiveRand(depth-1, p))),
-                   (0.333, () => Times(naiveRand(depth-1, p), naiveRand(depth-1, p)))))
+               Seq((0.333, () => Plus(naiveRand(depth - 1, p), naiveRand(depth - 1, p))),
+                   (0.333, () => Times(naiveRand(depth - 1, p), naiveRand(depth - 1, p)))))
   }
 
-  def balancedRand(depth:Int, p:Int): Expr = {
-    val nodeCount = random.between(1 << depth, 1 << (depth+1))
+  def balancedRand(depth: Int, p: Int): Expr = {
+    val nodeCount = random.between(1 << depth, 1 << (depth + 1))
     val population = scala.util.Random.shuffle((0 until nodeCount).to(List))
 
     abstract class Node
-    case class InternalNode(n:Int, left:Node, right:Node) extends Node
+    case class InternalNode(n: Int, left: Node, right: Node) extends Node
     case class LeafNode() extends Node
 
-    def insert(node:Node, a:Int):Node = {
+    def insert(node: Node, a: Int): Node = {
       node match {
         case InternalNode(n, left, right) if a < n => InternalNode(n, insert(left, a), right)
         case InternalNode(n, left, right) => InternalNode(n, left, insert(right, a))
         case LeafNode() => InternalNode(a, LeafNode(), LeafNode())
       }
     }
-    def nodeToExpr(node:Node): Expr = {
+
+    def nodeToExpr(node: Node): Expr = {
       node match {
         case InternalNode(_, left, right) =>
           randCase(() => Plus(nodeToExpr(left), nodeToExpr(right)),
-            Seq((0.5, () => Times(nodeToExpr(left), nodeToExpr(right)))))
+                   Seq((0.5, () => Times(nodeToExpr(left), nodeToExpr(right)))))
         case LeafNode() => Const(random.nextInt(p))
       }
     }
-    val default:Node = LeafNode()
+
+    val default: Node = LeafNode()
     nodeToExpr(population.foldLeft(default)(insert))
   }
 
 }
 
 object TestExpr {
-  import Expr.{eval,balancedRand, naiveRand}
-  import adjuvant.Adjuvant.measureFrequencies
-  def main(argv:Array[String]):Unit = {
-    val p = 7
-    println(measureFrequencies(10000,() => eval(balancedRand(3, p), p)).to(List).sortBy(_._1))
-    println(measureFrequencies(10000,() => eval(naiveRand(3,p),p)).to(List).sortBy(_._1))
+
+  import Expr.{eval, balancedRand, naiveRand}
+  import adjuvant.Adjuvant.returnPercentages
+
+  def main(argv: Array[String]): Unit = {
+    val p = 53
+    val m = 100000
+    println("Balanced", returnPercentages(m, () => eval(balancedRand(3, p), p)))
+    println("Naive   ", returnPercentages(m, () => eval(naiveRand(3, p), p)))
 
   }
 }
 
-object Mod {
-  def mul_mod(a:Int, b:Int, p:Int):Int = {
-    (a * b) % p
-  }
-
-  def frequencies[A](population:Seq[A]):(Map[A,Int],Map[Int,Set[A]]) = {
-    val fr = for{
-      (a,as) <- population.groupBy(identity)
-    } yield (a, as.length)
-
-    val revmap = fr.foldLeft(Map[Int,Set[A]]())((acc,item) => {
-      val (a:A,n:Int) = item
-      acc.get(n) match {
-        case None => acc + (n -> Set(a))
-        case Some(s) => acc + (n -> (s ++ Set(a)))
-        }
-    })
-
-    (fr, revmap)
-  }
-
-}
 
 object ModDemo {
 
-  def main(argv:Array[String]):Unit = {
-    val p = 15
-    def mul(a:Int, b:Int):Int = Mod.mul_mod(a,b,p)
+  // print a histogram of percentages of all the values
+  // of a*b mod p
+  def main(argv: Array[String]): Unit = {
+    val p = 11
 
-    val products = for{a <- 0 to p-1
-                       b <- 0 to p-1} yield mul(a,b)
+    val size = (p * p).toDouble
+
+    val products = (for {a <- 0 to p - 1
+                         b <- 0 to p - 1} yield (a, b))
+      .foldLeft(Map[Int, Int]()) {
+        case (acc, (a, b)) =>
+          val prod = a * b % p
+          acc + (prod -> (1 + acc.getOrElse(prod, 0)))
+      }
+      .to(List)
+      .map { case (prod, count) => (prod, 100 * count / size) }
+      .sortBy(_._2)
+      .reverse
     println(products)
-    val (fr, revmap) = Mod.frequencies(products)
-
-    println(revmap)
-    println(fr)
-
   }
 }
