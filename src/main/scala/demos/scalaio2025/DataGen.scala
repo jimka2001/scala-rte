ebd14b6f2463822bbc090d683cb14d2a24a137cb
Jim Newton <jim.newton@epita.fr>
2025-09-28 15:08:52 +0200

    fixed typo

--- src/main/scala/demos/scalaio2025/DataGen.scala
+++ src/main/scala/demos/scalaio2025/DataGen.scala
@@ -9,7 +9,7 @@ object DataGen {
 object GenCsvBalanced {
   def main(argv: Array[String]): Unit = {
     val num_repetitions:Int = if (argv.length == 0) 50 else argv(0).toInt
-    RteTree.genCsvBySize(num_repetitions, algo="naive")
+    RteTree.genCsvBySize(num_repetitions, algo="balanced")
   }
 }
 
